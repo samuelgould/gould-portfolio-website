cb123943c41c59fd896b88b7c8119944a0745aca
Sam Gould <sjg326@gmail.com>
2018-02-09 10:56:22 -0800

    added event listeners and faded out homepage background image

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+document.getElementById('portfolio-link').onclick = () => {
+  console.log('portfolio link has been clicked');
+};
+
+document.getElementById('about-link').onclick = () => {
+  console.log('about link has been clicked');
+};
+
+document.getElementById('contact-link').onclick = () => {
+  console.log('contact link has been clicked');
+};
+
+document.getElementById('homepage-link').onclick = () => {
+  console.log('homepage link has been clicked');
+};
